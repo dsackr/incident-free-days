6dd815c02f4b951b4cf3c5145988312b00a33b9f
dsackr <47996578+dsackr@users.noreply.github.com>
2026-01-06 23:34:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -622,7 +622,7 @@ def generate_osha_sign(auto_display=False, incidents=None):
         asterisk_width = asterisk_bbox[2] - asterisk_bbox[0]
         asterisk_height = asterisk_bbox[3] - asterisk_bbox[1]
         asterisk_x = prior_x + prior_width + 6
-        asterisk_y = prior_y - (asterisk_height // 2) + 20
+        asterisk_y = prior_y - (asterisk_height // 2) + 50
         draw.text((asterisk_x, asterisk_y), asterisk_text, font=asterisk_font, fill="white")
 
     inc_text = data.get("incident_number", "")
